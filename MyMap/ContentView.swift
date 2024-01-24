06c0675a95b002ccfff7a2673bdbc86ce7c7efbf
gadgelogger <sarayou273142910@gmail.com>
2024-01-24 10:27:40 +0900

    地図の種類を変更するボタンと処理を追加

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    //入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    //検索キーワードを保持する状態変数、初期値は"東京駅"
+    @State var displaySearchKey: String = "東京駅"
+    //マップ種類　最初は標準から
+    @State var displayMapType: MapType = .standard
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    //垂直にレイアウト
+        VStack{
+            //テキストフィールド(文字入力)
+            TextField("キーワード",text: $inputText,prompt:
+            Text("キーワードを入力してください"))
+            //入力が完了された時
+            .onSubmit {
+                //入力が完了したので検索キーワードに設定する
+                displaySearchKey = inputText
+            }
+            //余白を追加
+            .padding()
+            //奥から手前方向にレイアウト(右下基準で配置する)
+            ZStack(alignment:.bottomTrailing){
+                //マップを表示
+                MapView(searchKey: displaySearchKey,mapType: displayMapType)
+                //マップ種類切り替えボタン
+                Button {
+                    //標準　→　衛星写真　→　衛星写真と交通機関ラベル
+                    if displayMapType == .standard{
+                        displayMapType = .satellite
+                    } else if displayMapType == .satellite {
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                } label: {
+                    //マップアイコンの表示
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0,height: 35.0)
+                }//buttonここまで
+                //右の余白を20
+                .padding(.trailing,20.0)
+                //下の余白を30
+                .padding(.bottom,30.0)
+            }
+            
         }
-        .padding()
     }
 }
 
--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
+
+//画面で選択したマップの種類を示す列挙型
+enum MapType{
+    case standard //通常
+    case satellite //衛星写真
+    case hybrid //衛星写真と交通機関ラベル
+}
+
+
+struct MapView: View{
+    //検索キーワード
+    let searchKey: String
+    //マップ種類
+    let mapType: MapType
+    //キーワードから取得した緯度経度
+    @State var targetCoordinate = CLLocationCoordinate2D()
+    //表示するマップの位置
+    @State var cameraPositon: MapCameraPosition = .automatic
+    //表示するマップのスタイル
+    var mapStyle: MapStyle{
+        switch mapType {
+        case .standard:
+            return MapStyle.standard
+        case .satellite:
+            return MapStyle.imagery
+        case .hybrid:
+            return MapStyle.hybrid
+        }
+    }
+    var body: some View{
+        Map(position: $cameraPositon){
+            //マップにピンを表示
+            Marker(searchKey,coordinate: targetCoordinate)
+        }
+        //マップのスタイルを指定
+        .mapStyle(mapStyle)
+        //検索キーワードの変更を検知
+        .onChange(of: searchKey, initial: true){ oldValue, newValue
+            in
+            //入力されたキーワードをデバッグエリアに表示
+            print("検索キーワード:\(newValue)")
+            
+            //地図の検索クエリの作成
+            let request = MKLocalSearch.Request()
+            //検索クエリにキーワードの設定
+            request.naturalLanguageQuery = newValue
+            //MKLocalSearchの初期化
+            let search = MKLocalSearch(request: request)
+            //検索の開始
+            search.start{response,error in
+            //結果が存在する時は、1件目を取り出す
+                if let mapItems = response?.mapItems,
+                   let mapItem = mapItems.first{
+                    //位置情報から緯度経度をtargetCoordinateに取り出す
+                    targetCoordinate = mapItem.placemark.coordinate
+                    //緯度経度をデバッグエリアに表示
+                    print("緯度経度:\(targetCoordinate)")
+                    //表示するマップの領域を作成
+                    cameraPositon = .region(MKCoordinateRegion(
+                        center : targetCoordinate,
+                        latitudinalMeters: 500.0,
+                        longitudinalMeters: 500.0
+                    ))
+                }
+            }
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    MapView(searchKey: "東京駅",mapType: .standard)
+}
